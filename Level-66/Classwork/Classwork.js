f1399d9129a512170e3675fc80bed1eaabf73349
laghosha-19 <lagoshvili.luka@mail.ru>
2025-03-09 00:42:49 +0400

    commit

---
+++ Level-66/Classwork/Classwork.js
@@ -0,0 +1,20 @@
+let g = ture || ture
+let a = true || false
+let q = false || false
+
+let f = true && true 
+let v = true && false
+let r = fasle && false
+
+console.log(g);
+console.log(a);
+console.log(q);
+console.log(f);
+console.log(v);
+console.log(r);
+
+
+
+
+
+
