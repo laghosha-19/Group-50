34b6b320d6dee4d2a1384f11697a8afb8160fabd
laghosha-19 <laghoshvili.luka@gmail.com>
2025-05-29 19:04:22 +0400

    commit

--- Level-097/Homework/Homework.js
+++ Level-097/Homework/Homework.js
@@ -1,7 +1,16 @@
     let time = document.querySelector(".time")
+
     let same = document.querySelectorAll(".same")
+
     let clock = document.querySelector("#clock")
 
+    let rate = document.querySelector("#rate")
+
+    let TimeBut = document.querySelector("#TimeBut")
+
+    let HeartBut = document.querySelector("#HeartBut")
+
+    let login = document.querySelector(".fa-solid")
 
     let address = ["images/black.png","images/red.png","images/purple.png","images/blue.png"]
 
@@ -25,3 +34,17 @@ setInterval(() => {
 
     time.textContent = `${hour}:${minute}:${seconds}`
 }, 1000)
+
+HeartBut.addEventListener("click", () => {
+    time.style.display = "none"
+    rate.style.display = "flex"
+})
+
+TimeBut.addEventListener("click", () => {
+    rate.style.display = "none"
+    time.style.display = "flex"
+})
+
+login.addEventListener("click", () => {
+    
+})
