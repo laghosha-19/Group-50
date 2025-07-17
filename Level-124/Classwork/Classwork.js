8a34ef05e05e24a21e9ea346744a3ce62eac32b4
laghosha-19 <laghoshvili.luka@gmail.com>
2025-07-17 19:03:00 +0400

    commit

---
+++ Level-124/Classwork/Classwork.js
@@ -0,0 +1,33 @@
+
+
+
+// const getdataFromBackend = () =>{
+//     const http = new XMLHttpRequest()
+//     http.onreadystatechange = function(){
+//         console.log(this.readyState);
+//         if (this.readyState = 4){
+//             if (this.status = 200){
+//                 let response = this.responseText
+
+//                 response.map((item)=>{
+//                     document.body.innerHTML += item.title
+//                 })
+//                 }
+//             }
+//             console.log(this.readyState);
+//         }
+//     }
+
+
+//     http.open("GET", "https://jsonplaceholder.typicode.com/todos")
+//     http.send()
+
+
+// }
+
+
+
+// getdataFromBackend()
+
+
+// https://jsonplaceholder.typicode.com/todos
